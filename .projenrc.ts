1f971323d1b72a357ea25800f44d4ba1c30bfe8f
John Ferlito <johnf@inodes.org>
2024-01-13 15:38:17 +1100

    Restore aws-cdk version

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,7 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'John Ferlito',
   authorAddress: 'johnf@inodes.org',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.58.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.0.0',
   name: 'cdk-cross-account-route53',
