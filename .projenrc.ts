7d284c599d913406f98ad96dec402bbe54b3f3d1
John Ferlito <johnf@inodes.org>
2024-01-13 15:05:28 +1100

    Upgrade projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,10 +1,12 @@
-const { awscdk } = require('projen');
+import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'John Ferlito',
   authorAddress: 'johnf@inodes.org',
-  cdkVersion: '2.58.0',
+  cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.0.0',
   name: 'cdk-cross-account-route53',
+  projenrcTs: true,
   description: 'CDK Construct to allow creation of Route 53 records in a different account',
   repositoryUrl: 'https://github.com/johnf/cdk-cross-account-route53.git',
   keywords: [
