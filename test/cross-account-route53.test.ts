3ad4911947ef63a240c47d4c12a68dbe22b21796
John Ferlito <johnf@inodes.org>
2024-01-15 16:44:47 +1100

    Fix tests

--- test/cross-account-route53.test.ts
+++ test/cross-account-route53.test.ts
@@ -41,7 +41,7 @@ test('Role Created (defaults)', () => {
 
   expect(condition['ForAllValues:StringEquals']['route53:ChangeResourceRecordSetsRecordTypes']).toEqual(['A', 'AAAA']);
   expect(condition['ForAllValues:StringEquals']['route53:ChangeResourceRecordSetsActions']).toEqual(['CREATE', 'UPSERT', 'DELETE']);
-  expect(condition['ForAllValues:StringEquals']['route53:ChangeResourceRecordSetsNormalizedRecordNames']).toEqual(['test.inodes.org']);
+  expect(condition['ForAllValues:StringLike']['route53:ChangeResourceRecordSetsNormalizedRecordNames']).toEqual(['test.inodes.org']);
 });
 
 test('Role Created (custom)', () => {
@@ -81,5 +81,46 @@ test('Role Created (custom)', () => {
 
   expect(condition['ForAllValues:StringEquals']['route53:ChangeResourceRecordSetsRecordTypes']).toEqual(['TXT']);
   expect(condition['ForAllValues:StringEquals']['route53:ChangeResourceRecordSetsActions']).toEqual(['UPSERT']);
-  expect(condition['ForAllValues:StringEquals']['route53:ChangeResourceRecordSetsNormalizedRecordNames']).toEqual(['test\\045.inodes.org']);
+  expect(condition['ForAllValues:StringLike']['route53:ChangeResourceRecordSetsNormalizedRecordNames']).toEqual(['test\\045.inodes.org']);
+});
+
+test('Role Created (not normalised)', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, 'TestStack');
+
+  const props: CrossAccountRoute53RoleProps = {
+    roleName: 'TestRole',
+    assumedBy: new iam.AccountPrincipal('123456789012'),
+    zone: new route53.HostedZone(stack, 'HostedZone', { zoneName: 'inodes.org' }),
+    records: [
+      { domainNames: '*.inodes.org' },
+    ],
+    normaliseDomains: false,
+  };
+
+  new CrossAccountRoute53Role(stack, 'MyTestConstruct', props);
+
+  const template = Template.fromStack(stack);
+  const policies = new Capture();
+  // const assumedBy = new Capture();
+
+  template.hasResourceProperties('AWS::IAM::Role', {
+    RoleName: 'TestRole',
+    Policies: policies,
+    // AssumeRolePolicyDocument: assumedBy,
+  });
+  // console.debug(assumedBy.asString());
+
+  const policy = policies.asArray()[0];
+  const statements = policy.PolicyDocument.Statement;
+
+  const recordStatement = statements.find((statement: any) => statement.Action === 'route53:ChangeResourceRecordSets');
+
+  expect(recordStatement).toBeDefined();
+  const condition = recordStatement.Condition;
+  expect(condition).toBeDefined();
+
+  expect(condition['ForAllValues:StringEquals']['route53:ChangeResourceRecordSetsRecordTypes']).toEqual(['A', 'AAAA']);
+  expect(condition['ForAllValues:StringEquals']['route53:ChangeResourceRecordSetsActions']).toEqual(['CREATE', 'UPSERT', 'DELETE']);
+  expect(condition['ForAllValues:StringLike']['route53:ChangeResourceRecordSetsNormalizedRecordNames']).toEqual(['*.inodes.org']);
 });
